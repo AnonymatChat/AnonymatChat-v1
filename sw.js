b390d38c2e17e460d0f83f783ef09ce135487cfa
AnonymatChat <tes44536@gmail.com>
2026-02-05 22:52:04 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('push', function(event) {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png', // Icône bulle
+        badge: 'https://cdn-icons-png.flaticon.com/512/1041/1041916.png',
+        vibrate: [100, 50, 100],
+        data: { url: data.url }
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
+
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+});
